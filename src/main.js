329154d6ce1d53e5f6fffb99ec9e193aced1e336
bebriks <114162901+bebriks@users.noreply.github.com>
2024-02-25 22:22:08 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
-import ListFilterElement from "./view/filter-view";
-import {render, RenderPosition} from "./render";
-import ListSortElement from "./view/sort-view";
-import ListElement from "./view/point-view";
-import NewPoint from "./view/add-new-point";
-import EventListView from "./view/event-list-view";
-import BoardPresenter from "./presenter/board-presenter";
-import MainInfo from "./view/info-view";
+import ListFilterElement from './view/filter-view';
+import {render, RenderPosition} from './render';
+import BoardPresenter from './presenter/board-presenter';
+import MainInfo from './view/info-view';
 
-const bodyElement = document.querySelector("body");
-const headerElement = bodyElement.querySelector(".page-header");
-const siteListFilter = headerElement.querySelector(".trip-controls__filters");
-const tripMain = headerElement.querySelector(".trip-main");
-const eventsList = bodyElement.querySelector(".trip-events");
+const bodyElement = document.querySelector('body');
+const headerElement = bodyElement.querySelector('.page-header');
+const siteListFilter = headerElement.querySelector('.trip-controls__filters');
+const tripMain = headerElement.querySelector('.trip-main');
+const eventsList = bodyElement.querySelector('.trip-events');
 
 const boardPresenter = new BoardPresenter({
   container: eventsList
